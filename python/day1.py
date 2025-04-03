ee64e36cda790d3d4bb056a4c342a7273d40ced6
gitea <admin@admin.com>
2025-04-03 13:52:23 +0200

    primer dia aprendiendo python

---
+++ python/day1.py
@@ -0,0 +1,20 @@
+print("Hello world!")
+
+print("Hello world!\nHello world!")
+
+print("Hello" + " " + "world!")
+
+input("What is your name?")
+
+print("Hello " + input("What is your name?"))
+
+# Para comentar se utilizan los hastags.
+
+#Guarda el input en una variable
+name = input("What is your name?")
+
+#Mostrar por pantalla la variable
+print(name)
+
+#Cuantos caracteres tiene el contenido de la variable
+print(len(name))
