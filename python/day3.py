80aeed792cc60b6324cba58a7c8a069240b0de24
gitea <admin@admin.com>
2025-04-07 17:59:24 +0200

    condicionales y operadores

---
+++ python/day3.py
@@ -0,0 +1,48 @@
+height = 180
+
+if height > 120:
+    print("Puedes subirte")
+else:
+    print("No puedes subir")
+
+
+#> mayor Puedes
+#< menor que
+#>= mayor o igual que
+#<= menor o igual que
+#== igual 
+#!= distinto
+
+
+#Modulo es el resto de una division. Para poner el modulo se usa %
+
+if 12 % 2 == 0:
+    print("even number")
+else:
+    print("odd number")
+
+
+#nested if / else
+#if condition:
+#    if another condition:
+#        do this
+#    else:
+#        do this
+#else:
+#    do this
+#
+
+# varios if / elif / else
+#
+# if condition1:
+#   do a
+#elif condition2:
+#   do b
+#else:
+#   do c
+#
+
+# Logicals operators
+# A and B
+# A or B
+# not A
